8a7f5aa1306c9ea66264e569ac927113085751ee
K Ryan <hunt.kelsearyan@gmail.com>
2021-03-16 22:59:03 -0600

    Restructured the HTML

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+  
 var today = moment();
 
 var dayWeek = today.format("dddd, MMMM Do");
@@ -5,43 +6,15 @@ $("#currentDay").text(dayWeek);
 
 var timeEl = $("#time");
 var inputEl = $("#input");
-var buttonEl = $("#saveBtn");
-
-var timeblocks = [
-  moment().hour(9).format('HH:00'),
-  moment().hour(10).format('HH:00'),
-  moment().hour(11).format('HH:00'),
-  moment().hour(12).format('HH:00'),
-  moment().hour(13).format('HH:00'),
-  moment().hour(14).format('HH:00'),
-  moment().hour(15).format('HH:00'),
-  moment().hour(16).format('HH:00'),
-  moment().hour(17).format('HH:00')
-];
-
-for (var i = 0; i < timeblocks.length; i++) {
-  var timeblocksEl = $("<tr>");
-  timeblocksEl.text(timeblocks[i]);
-  timeblocksEl.addClass("time-block row hour");
-  timeEl.append(timeblocksEl);
-
-  var inputRowEl = $("<textarea>");
-  inputRowEl.addClass("col-12 row description");
-  inputEl.append(inputRowEl);
-
-  var saveBtnEl = $("<button>");
-  saveBtnEl.text('BUTTON');
-  saveBtnEl.addClass("saveBtn row");
-  buttonEl.append(saveBtnEl);
-}
+var buttonEl = $(".saveBtn");
 
 function checkTime() {
   var currentTime = moment().hour('').format("HH:00");
-  var timeBlockHours = $(".description");
+  var timeBlockHours = $(".hour");
 
-  for (var i = 0; i < timeBlockHours.length; i++) {
-    var timeElement = timeBlockHours[i].id;
-    var manipID = document.getElementById(timeBlockHours[i].id);
+  for (var i = 0; i < timeBlockHours.value; i++) {
+    var timeElement = timeBlockHours[i].value;
+    var manipID = document.getElementById(timeBlockHours[i].value);
     $(timeBlockHours[i].id).removeClass("hour");
     if (timeElement < currentTime) {
       $(manipID).addClass("past");
@@ -53,14 +26,12 @@ function checkTime() {
   }
 }
 
-setInterval(checkTime(), 1000 * 60 * 5);
+var userTask = document.querySelector('.description');
 
-var userTask = document.querySelector('.col-12.row.description');
-
-$('.saveBtn').click(function saveData(event) {
+buttonEl.on('click', function saveData(event) {
   event.preventDefault();
   var taskAtHand = {
-    task: userTask.value.trim()
+    task: userTask.value
   };
   localStorage.setItem('taskAtHand', JSON.stringify(taskAtHand));
   renderLastTasks();
@@ -69,6 +40,6 @@ $('.saveBtn').click(function saveData(event) {
 function renderLastTasks() {
   var lastTasks = JSON.parse(localStorage.getItem("taskAtHand"));
   if (lastTasks !== null) {
-    document.querySelector(".col-12.row.description").textContent = lastTasks.task
+    document.querySelectorAll(".description").textContent = lastTasks.task
   }
 }
